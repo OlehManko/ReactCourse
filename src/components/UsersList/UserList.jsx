ef01081a5b1dc6a6b1b850edae3f4cf8cdf66590
OlehManko <oleh.manko@beetroot.se>
2022-01-27 20:57:51 +0200

    Add filter list

--- src/components/UsersList/UserList.jsx
+++ src/components/UsersList/UserList.jsx
@@ -10,21 +10,32 @@ const SORT_PROP = [
 
 export const UserList = ({ users, deleteUser }) => {
   const [typeSort, setTypeSort] = useState("any");
+  const [line, setLine] = useState("");
 
   console.log("Re-rendering");
 
-  const sortedArray = sortList(typeSort, users);
+  const sortedArray = sortList(
+    typeSort,
+    users.filter((item) => item.name.toLowerCase().includes(line.toLowerCase()))
+  );
 
   return (
     <>
       <h2>Our emploees</h2>
-      <select value={typeSort} onChange={(e) => setTypeSort(e.target.value)}>
-        {SORT_PROP.map((item) => (
-          <option value={item.value} key={item.value}>
-            {item.label}
-          </option>
-        ))}
-      </select>
+      <div>
+        <select value={typeSort} onChange={(e) => setTypeSort(e.target.value)}>
+          {SORT_PROP.map((item) => (
+            <option value={item.value} key={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <input
+          type="search"
+          value={line}
+          onChange={(e) => setLine(e.target.value)}
+        />
+      </div>
       {users?.length ? (
         sortedArray.map((item, index) => (
           <UserItem
